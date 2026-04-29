c9186fd6c54a79c1fdffd9bc0de9ec5bf2023999
Kabirul Islam <kabirul.islam.co@gmail.com>
2026-04-30 01:26:41 +0530

    fix assets

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8cc79252c2d801cef88ce34b958e2563
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "45d1e1ca91fc25a6dba73dfb806291cc",
+"flutter_bootstrap.js": "3d91151fc857d69ca64e4ab30e5cff17",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
